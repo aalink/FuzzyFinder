a5e68bd5db75d7ec52a7c493607830a121550aa8
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-08-01 20:08:25 -0400

    Replaces React logo with dog image and <h1> Removing unneeded CSS code as well

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import fuzzyfriend from './assets/images/fuzzyfriend.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={fuzzyfriend} className="App-logo" alt='Fuzzy Friend'/>
+        <h1>FuzzyFinder</h1>
+
       </header>
     </div>
   );
